ab1f4474d6f7c9b7191150ea26f631edeeecf092
man.wang <man.wang@symbio.com>
2022-07-27 22:48:58 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "./app.scss";
+
+class App extends Component {
+  componentDidMount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  componentDidCatchError() {}
+
+  // this.props.children 是将要会渲染的页面
+  render() {
+    const a = "1";
+    return this.props.children;
+  }
+}
+
+export default App;
